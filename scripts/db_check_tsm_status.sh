52bbaa16ddd594bc0c3f49fca367c43245c8de23
Ignatius Stanislaus <stanize@gmail.com>
2026-03-08 13:53:42 +0100

    Update db_check_tsm_status.sh - tsm STATUS

--- scripts/db_check_tsm_status.sh
+++ scripts/db_check_tsm_status.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-JBOSS_HOME="/mnt/temenos/3rdParty/AS/jboss-eap-7.4"
-DEPLOY_DIR="$JBOSS_HOME/standalone/deployments"
+STATUS=$(psql -h T24-DB -U t24 -d BANCA -t -A -c \
+"SELECT COALESCE((xmlrecord::json)->>'6','')
+ FROM public.\"F_TSA_SERVICE\"
+ WHERE recid = 'TSM';")
 
-# 1. Check if JBoss process is running
-JBOSS_PID=$(pgrep -f "jboss-modules")
+STATUS=$(echo "$STATUS" | xargs)
 
-if [ -z "$JBOSS_PID" ]; then
-    echo "STOPPED"
-    exit 1
-fi
-
-# 2. Check deployment folder
-failed=$(ls $DEPLOY_DIR/*.failed 2>/dev/null | wc -l)
-deploying=$(ls $DEPLOY_DIR/*.isdeploying 2>/dev/null | wc -l)
-pending=$(ls $DEPLOY_DIR/*.dodeploy 2>/dev/null | wc -l)
-
-if [ "$failed" -gt 0 ]; then
-    echo "LOADING"
-    exit 2
-fi
-
-if [ "$deploying" -gt 0 ] || [ "$pending" -gt 0 ]; then
-    echo "LOADING"
-    exit 2
-fi
-
-echo "RUNNING"
-exit 0
+case "$STATUS" in
+    START|AUTO)
+        echo "RUNNING"
+        ;;
+    STOP)
+        echo "STOPPED"
+        ;;
+    *)
+        echo "STOPPED"
+        ;;
+esac
