db695814adc5171e0c435edc98a045986426f473
Ignatius Stanislaus <stanize@gmail.com>
2026-03-08 15:01:51 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -11,4 +11,4 @@ git fetch origin
 git reset --hard origin/main
 
 echo "Starting Streamlit dashboard..."
-streamlit run app.py --server.port 8800 --server.address 0.0.0.0
+streamlit run app.py --server.port 8800
