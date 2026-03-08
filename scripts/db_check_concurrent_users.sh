03a7ab54bff3d143c17a46fec6cd17e7ca5ada84
Ignatius Stanislaus <stanize@gmail.com>
2026-03-08 14:07:42 +0100

    Update db_check_concurrent_users.sh MAX Users

--- scripts/db_check_concurrent_users.sh
+++ scripts/db_check_concurrent_users.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
-JBOSS_HOME="/mnt/temenos/3rdParty/AS/jboss-eap-7.4"
-DEPLOY_DIR="$JBOSS_HOME/standalone/deployments"
-
-# 1. Check if JBoss process is running
-JBOSS_PID=$(pgrep -f "jboss-modules")
-
-if [ -z "$JBOSS_PID" ]; then
-    echo "STOPPED"
-    exit 1
-fi
-
-# 2. Check deployment folder
-failed=$(ls $DEPLOY_DIR/*.failed 2>/dev/null | wc -l)
-deploying=$(ls $DEPLOY_DIR/*.isdeploying 2>/dev/null | wc -l)
-pending=$(ls $DEPLOY_DIR/*.dodeploy 2>/dev/null | wc -l)
-
-if [ "$failed" -gt 0 ]; then
-    echo "LOADING"
-    exit 2
-fi
-
-if [ "$deploying" -gt 0 ] || [ "$pending" -gt 0 ]; then
-    echo "LOADING"
-    exit 2
-fi
-
-echo "RUNNING"
-exit 0
+psql -h T24-DB -U t24 -d BANCA -t -A -c \
+'SELECT COUNT(*) FROM "public"."F_OS_TOKEN";'
