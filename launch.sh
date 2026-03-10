968a933e8721c604eb0f1842c7f2da7c48ed3e85
Ignatius Stanislaus <stanize@gmail.com>
2026-03-10 17:44:10 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -10,5 +10,8 @@ echo "Pulling latest version from GitHub..."
 git fetch origin
 git reset --hard origin/main
 
+echo "Activating virtual environment..."
+source venv/bin/activate
+
 echo "Starting Streamlit dashboard..."
 streamlit run app.py --server.port 8800
